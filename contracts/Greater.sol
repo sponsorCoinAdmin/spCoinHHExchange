64ccc83d3676692760d603d65b44306c8f3b314c
sponsorCoinAdmin <sponsorcoin@protonmail.com>
2022-10-05 02:38:56 -0400

    Initial Release

---
+++ contracts/Greater.sol
@@ -0,0 +1,24 @@
+
+pragma solidity ^0.7.6;
+
+import "hardhat/console.sol";
+
+contract Greeter  {
+    string private greeting;  // data
+
+    
+    /* This runs when the contract is executed */
+   constructor(string memory _greeting) {
+        console.log("Deploying a Greeter with greeting:", _greeting);
+        greeting = _greeting;
+    } 
+    
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+    
+    function setGreeting(string memory _greeting) public {
+        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+        greeting = _greeting;
+    }
+}
