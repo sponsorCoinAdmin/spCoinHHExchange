02a761469e3603cc374344acdfbe401d4c02a091
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-09-20 02:50:11 -0400

    WIP

--- prod/spCoinExchange.js
+++ prod/spCoinExchange.js
@@ -1,73 +1,83 @@
 class SpCoinExchange {
-    constructor(ethers, spCoinExchangeContract, accounts) {
-      this.ethers = ethers,
-      this.spCoinExchangeContract = spCoinExchangeContract;
-      this.accounts = accounts;
-      this.tokenInContract;
-      this.tokenOutContract;
-      this.indent = "    ";
-    }
-
-    async swapExactInputSingle(
-        _tokenIn,
-        _tokenOut,
-        _poolFee,
-        _amountIn,
-        _amountOutMin,
-        _sqrtPriceLimitX96) {
-            await this.spCoinExchangeContract.swapExactInputSingle(
-                _tokenIn, 
-                _tokenOut, 
-                _poolFee, 
-                _amountIn, 
-                _amountOutMin, 
-                _sqrtPriceLimitX96
-            );
-    }
-
-    async logSwapExactInputSingle(
-        _tokenInName,
-        _tokenOutName,
-        _tokenIn,
-        _tokenOut,
-        _poolFee,
-        _amountIn,
-        _amountOutMin,
-        _sqrtPriceLimitX96) {
-
-          let accounts = this.accounts;
-          let tokenInContract = this.tokenInContract;
-          let tokenOutContract = this.tokenOutContract;
-          let indent = this.indent;
-    
-          let beforeTokenInBalanceOf = await tokenInContract.balanceOf(accounts[0].address);
-          let beforeTokenOutBalanceOf = await tokenOutContract.balanceOf(accounts[0].address);
-    
-          console.log(indent + "BEFORE TOKEN_IN  ~", _tokenInName, "balance:", beforeTokenInBalanceOf);
-          console.log(indent + "BEFORE TOKEN_OUT ~", _tokenOutName, " balance:", beforeTokenOutBalanceOf);
-            
-          // Swap Exact Input Single
-          await this.swapExactInputSingle(
-            _tokenIn, 
-            _tokenOut,
-            _poolFee,
-            _amountIn,
-            _amountOutMin,
-            _sqrtPriceLimitX96
-          );
-          
-          let afterTokenInBalanceOf = await tokenInContract.balanceOf(accounts[0].address);
-          let afterTokenOutBalanceOf = await tokenOutContract.balanceOf(accounts[0].address);
-    
-          console.log(indent + "AFTER TOKEN_IN   ~", _tokenInName, "balance:", afterTokenInBalanceOf);
-          console.log(indent + "AFTER TOKEN_OUT  ~", _tokenOutName, " balance:", afterTokenOutBalanceOf);
-    
-          console.log(indent + "DIFFERENCE       ~", _tokenInName, BigInt(afterTokenInBalanceOf) - BigInt(beforeTokenInBalanceOf));
-          console.log(indent + "DIFFERENCE       ~", _tokenOutName, BigInt(afterTokenOutBalanceOf)  - BigInt(beforeTokenOutBalanceOf));
-      }
-
+  constructor(ethers, spCoinExchangeContract, accounts) {
+    this.ethers = ethers,
+    this.spCoinExchangeContract = spCoinExchangeContract;
+    this.accounts = accounts;
+    this.spCoinContract;
+    this.tokenInContract;
+    this.tokenOutContract;
+    this.indent = "    ";
   }
 
-  module.exports = {
-    SpCoinExchange
-  };
+  async swapExactInputSingle(
+    _tokenIn,
+    _tokenOut,
+    _poolFee,
+    _amountIn,
+    _amountOutMin,
+    _sqrtPriceLimitX96) {
+      await this.spCoinExchangeContract.swapExactInputSingle(
+        _tokenIn, 
+        _tokenOut, 
+        _poolFee, 
+        _amountIn, 
+        _amountOutMin, 
+        _sqrtPriceLimitX96
+      );
+  }
+
+  async deployContract(contract) {
+    this.accounts = await ethers.getSigners(1);
+    const contractFactory = await ethers.getContractFactory(contract);
+    this.spCoinContract = await contractFactory.deploy();
+    await spCoinContract.deployed();
+  }
+
+  async logSwapExactInputSingle (
+    _tokenInName,
+    _tokenOutName,
+    _tokenIn,
+    _tokenOut,
+    _tokenInABI,
+    _tokenOutABI,
+    _poolFee,
+    _amountIn,
+    _amountOutMin,
+    _sqrtPriceLimitX96) {
+
+      let accounts = this.accounts;
+      let indent = this.indent;
+
+      let tokenOutContract = await ethers.getContractAt(_tokenOutABI, _tokenOut);
+      let tokenInContract = await ethers.getContractAt(_tokenInABI, _tokenIn);
+
+      let beforeTokenInBalanceOf = await tokenInContract.balanceOf(accounts[0].address);
+      let beforeTokenOutBalanceOf = await tokenOutContract.balanceOf(accounts[0].address);
+
+      console.log(indent + "BEFORE TOKEN_IN  ~", _tokenInName, "balance:", beforeTokenInBalanceOf);
+      console.log(indent + "BEFORE TOKEN_OUT ~", _tokenOutName, " balance:", beforeTokenOutBalanceOf);
+        
+      // Swap Exact Input Single
+      await this.swapExactInputSingle(
+        _tokenIn, 
+        _tokenOut,
+        _poolFee,
+        _amountIn,
+        _amountOutMin,
+        _sqrtPriceLimitX96
+      );
+      
+      let afterTokenInBalanceOf = await tokenInContract.balanceOf(accounts[0].address);
+      let afterTokenOutBalanceOf = await tokenOutContract.balanceOf(accounts[0].address);
+
+      console.log(indent + "AFTER TOKEN_IN   ~", _tokenInName, "balance:", afterTokenInBalanceOf);
+      console.log(indent + "AFTER TOKEN_OUT  ~", _tokenOutName, " balance:", afterTokenOutBalanceOf);
+
+      console.log(indent + "DIFFERENCE       ~", _tokenInName, BigInt(afterTokenInBalanceOf) - BigInt(beforeTokenInBalanceOf));
+      console.log(indent + "DIFFERENCE       ~", _tokenOutName, BigInt(afterTokenOutBalanceOf)  - BigInt(beforeTokenOutBalanceOf));
+  }
+}
+
+module.exports = {
+  SpCoinExchange
+};
--- test/swapExactInputSingle.js
+++ test/swapExactInputSingle.js
@@ -3,163 +3,124 @@ const { SpCoinExchange } = require("../prod/spCoinExchange");
 
 describe("SwapExactInputSingleTest: Swaps exact amount of _tokenIn for a maximum possible amount of _tokenOut"
 , function () {
-  console.log("SwapExactInputSingleTest:");
+    console.log("SwapExactInputSingleTest:");
 
-  let spCoinContract
-  let spCoinExchange
-  let accounts
-  let tokenInContract
-  const indent = "    ";
+    let spCoinContract
+    let spCoinExchange
+    let accounts
+    const indent = "    ";
 
-  // Before Initialization
-  before(async () => {
-    accounts = await ethers.getSigners(1);
-    await deployContract("SpCoinExchange");
-  })
+    // Before Initialization
+    before(async () => {
+      await deployContract(ethers, "SpCoinExchange");
+      spCoinExchange = new SpCoinExchange(ethers, spCoinContract, accounts);
+    })
 
-  async function deployContract(contract) {
-    const contractFactory = await ethers.getContractFactory(contract);
-    spCoinContract = await contractFactory.deploy();
-    await spCoinContract.deployed();
-    spCoinExchange = new SpCoinExchange(ethers,spCoinContract, accounts);
-  }
-
-  async function logSwapExactInputSingle(
-    _tokenInName,
-    _tokenOutName,
-    _tokenIn,
-    _tokenOut,
-    _tokenInABI,
-    _tokenOutABI,
-    _poolFee,
-    _amountIn,
-    _amountOutMin,
-    _sqrtPriceLimitX96) {
-
-      let tokenOutContract = await ethers.getContractAt(_tokenOutABI, _tokenOut);
-
-      let beforeTokenInBalanceOf = await tokenInContract.balanceOf(accounts[0].address);
-      let beforeTokenOutBalanceOf = await tokenOutContract.balanceOf(accounts[0].address);
-
-      console.log(indent + "BEFORE TOKEN_IN  ~", _tokenInName, "balance:", beforeTokenInBalanceOf);
-      console.log(indent + "BEFORE TOKEN_OUT ~", _tokenOutName, " balance:", beforeTokenOutBalanceOf);
-        
-      // Swap Exact Input Single
-      await spCoinExchange.swapExactInputSingle(
-        _tokenIn, 
-        _tokenOut,
-        _poolFee,
-        _amountIn,
-        _amountOutMin,
-        _sqrtPriceLimitX96
-      );
-      
-      let afterTokenInBalanceOf = await tokenInContract.balanceOf(accounts[0].address);
-      let afterTokenOutBalanceOf = await tokenOutContract.balanceOf(accounts[0].address);
-
-      console.log(indent + "AFTER TOKEN_IN   ~", _tokenInName, "balance:", afterTokenInBalanceOf);
-      console.log(indent + "AFTER TOKEN_OUT  ~", _tokenOutName, " balance:", afterTokenOutBalanceOf);
-
-      console.log(indent + "DIFFERENCE       ~", _tokenInName, BigInt(afterTokenInBalanceOf) - BigInt(beforeTokenInBalanceOf));
-      console.log(indent + "DIFFERENCE       ~", _tokenOutName, BigInt(afterTokenOutBalanceOf)  - BigInt(beforeTokenOutBalanceOf));
-  }
-
-  // Test - swapExactInputSingleTest
-  it("swapExactInputSingleTest: WETH -> DAI", async function () {
-
-    const TOKEN_IN_ABI = require('../contracts/interfaces/WETH_ABI.json')
-    const TOKEN_OUT_ABI = require('../contracts/interfaces/ERC20_ABI.json')
-    
-    const TOKEN_IN_NAME = "WETH";
-    const TOKEN_OUT_NAME = "DAI";
-    const AMOUNT_IN = 10n ** 18n;
-    const TOKEN_IN = process.env.GOERLI_WETH;
-    const TOKEN_OUT = process.env.GOERLI_DAI;
-    const POOL_FEE = 3000;
-    const AMOUNT_OUT_MINIMUM = 0;
-    const SQRT_ROOT_PRICE_LIMIT_X96 = 0;
-
-    tokenInContract = await ethers.getContractAt(TOKEN_IN_ABI, TOKEN_IN);
-
-    // Deposit TOKEN_IN
-    await tokenInContract.connect(accounts[0]).deposit({ value: AMOUNT_IN });
-    await tokenInContract.connect(accounts[0]).approve(spCoinContract.address, AMOUNT_IN);
-
-    await logSwapExactInputSingle(
-      TOKEN_IN_NAME,
-      TOKEN_OUT_NAME,
-      TOKEN_IN,
-      TOKEN_OUT,
-      TOKEN_IN_ABI,
-      TOKEN_OUT_ABI,
-      POOL_FEE,
-      AMOUNT_IN,
-      AMOUNT_OUT_MINIMUM,
-      SQRT_ROOT_PRICE_LIMIT_X96);
-  }).timeout(100000);
+    async function deployContract(ethers, contract) {
+      accounts = await ethers.getSigners(1);
+      const contractFactory = await ethers.getContractFactory(contract);
+      spCoinContract = await contractFactory.deploy();
+      await spCoinContract.deployed();
+    }
 
     // Test - swapExactInputSingleTest
-  it("swapExactInputSingleTest: WETH -> SPCOIN", async function () {
+    it("swapExactInputSingleTest: WETH -> DAI", async function () {
 
-    const TOKEN_IN_ABI = require('../contracts/interfaces/WETH_ABI.json')
-    const TOKEN_OUT_ABI = require('../contracts/interfaces/ERC20_ABI.json')
-    
-    const TOKEN_IN_NAME = "WETH";
-    const TOKEN_OUT_NAME = "SPCOIN";
-    const AMOUNT_IN = 10n ** 18n;
-    const TOKEN_IN = process.env.GOERLI_WETH;
-    const TOKEN_OUT = process.env.GOERLI_SPCOIN;
-    const POOL_FEE = 3000;
-    const AMOUNT_OUT_MINIMUM = 0;
-    const SQRT_ROOT_PRICE_LIMIT_X96 = 0;
+      const TOKEN_IN_ABI = require('../contracts/interfaces/WETH_ABI.json')
+      const TOKEN_OUT_ABI = require('../contracts/interfaces/ERC20_ABI.json')
+      
+      const TOKEN_IN_NAME = "WETH";
+      const TOKEN_OUT_NAME = "DAI";
+      const AMOUNT_IN = 10n ** 18n;
+      const TOKEN_IN = process.env.GOERLI_WETH;
+      const TOKEN_OUT = process.env.GOERLI_DAI;
+      const POOL_FEE = 3000;
+      const AMOUNT_OUT_MINIMUM = 0;
+      const SQRT_ROOT_PRICE_LIMIT_X96 = 0;
 
-    tokenInContract = await ethers.getContractAt(TOKEN_IN_ABI, TOKEN_IN);
+      let tokenInContract = await ethers.getContractAt(TOKEN_IN_ABI, TOKEN_IN);
 
-    // Deposit TOKEN_IN
-    await tokenInContract.connect(accounts[0]).deposit({ value: AMOUNT_IN });
-    await tokenInContract.connect(accounts[0]).approve(spCoinContract.address, AMOUNT_IN);
+      // Deposit TOKEN_IN
+      await tokenInContract.connect(accounts[0]).deposit({ value: AMOUNT_IN });
+      await tokenInContract.connect(accounts[0]).approve(spCoinContract.address, AMOUNT_IN);
 
-    await logSwapExactInputSingle(
-      TOKEN_IN_NAME,
-      TOKEN_OUT_NAME,
-      TOKEN_IN,
-      TOKEN_OUT,
-      TOKEN_IN_ABI,
-      TOKEN_OUT_ABI,
-      POOL_FEE,
-      AMOUNT_IN,
-      AMOUNT_OUT_MINIMUM,
-      SQRT_ROOT_PRICE_LIMIT_X96);
-  }).timeout(100000);
+      await spCoinExchange.logSwapExactInputSingle(
+        TOKEN_IN_NAME,
+        TOKEN_OUT_NAME,
+        TOKEN_IN,
+        TOKEN_OUT,
+        TOKEN_IN_ABI,
+        TOKEN_OUT_ABI,
+        POOL_FEE,
+        AMOUNT_IN,
+        AMOUNT_OUT_MINIMUM,
+        SQRT_ROOT_PRICE_LIMIT_X96);
+    }).timeout(100000);
 
-  // Test - swapExactInputSingleTest
-  it("swapExactInputSingleTest: SPCOIN -> WETH", async function () {
-    const TOKEN_IN_ABI = require('../contracts/interfaces/WETH_ABI.json')
-    const TOKEN_OUT_ABI = require('../contracts/interfaces/ERC20_ABI.json')
-    
-    const TOKEN_IN_NAME = "SPCOIN";
-    const TOKEN_OUT_NAME = "WETH";
-    const AMOUNT_IN = 10n ** 18n;
-    const TOKEN_IN = process.env.GOERLI_SPCOIN;
-    const TOKEN_OUT = process.env.GOERLI_WETH;
-    const POOL_FEE = 3000;
-    const AMOUNT_OUT_MINIMUM = 0;
-    const SQRT_ROOT_PRICE_LIMIT_X96 = 0;
+      // Test - swapExactInputSingleTest
+    it("swapExactInputSingleTest: WETH -> SPCOIN", async function () {
 
-    tokenInContract = await ethers.getContractAt(TOKEN_IN_ABI, TOKEN_IN);
+      const TOKEN_IN_ABI = require('../contracts/interfaces/WETH_ABI.json')
+      const TOKEN_OUT_ABI = require('../contracts/interfaces/ERC20_ABI.json')
+      
+      const TOKEN_IN_NAME = "WETH";
+      const TOKEN_OUT_NAME = "SPCOIN";
+      const AMOUNT_IN = 10n ** 18n;
+      const TOKEN_IN = process.env.GOERLI_WETH;
+      const TOKEN_OUT = process.env.GOERLI_SPCOIN;
+      const POOL_FEE = 3000;
+      const AMOUNT_OUT_MINIMUM = 0;
+      const SQRT_ROOT_PRICE_LIMIT_X96 = 0;
 
-    await tokenInContract.connect(accounts[0]).approve(spCoinContract.address, AMOUNT_IN);
 
-    await logSwapExactInputSingle(
-      TOKEN_IN_NAME,
-      TOKEN_OUT_NAME,
-      TOKEN_IN,
-      TOKEN_OUT,
-      TOKEN_IN_ABI,
-      TOKEN_OUT_ABI,
-      POOL_FEE,
-      AMOUNT_IN,
-      AMOUNT_OUT_MINIMUM,
-      SQRT_ROOT_PRICE_LIMIT_X96);
-  }).timeout(100000);  
+      // Deposit TOKEN_IN
+      let tokenInContract = await ethers.getContractAt(TOKEN_IN_ABI, TOKEN_IN);
+      await tokenInContract.connect(accounts[0]).deposit({ value: AMOUNT_IN });
+      await tokenInContract.connect(accounts[0]).approve(spCoinContract.address, AMOUNT_IN);
+
+      await spCoinExchange.logSwapExactInputSingle(
+        TOKEN_IN_NAME,
+        TOKEN_OUT_NAME,
+        TOKEN_IN,
+        TOKEN_OUT,
+        TOKEN_IN_ABI,
+        TOKEN_OUT_ABI,
+        POOL_FEE,
+        AMOUNT_IN,
+        AMOUNT_OUT_MINIMUM,
+        SQRT_ROOT_PRICE_LIMIT_X96);
+    }).timeout(100000);
+
+    // Test - swapExactInputSingleTest
+    it("swapExactInputSingleTest: SPCOIN -> WETH", async function () {
+      const TOKEN_IN_ABI = require('../contracts/interfaces/WETH_ABI.json')
+      const TOKEN_OUT_ABI = require('../contracts/interfaces/ERC20_ABI.json')
+      
+      const TOKEN_IN_NAME = "SPCOIN";
+      const TOKEN_OUT_NAME = "WETH";
+      const AMOUNT_IN = 10n ** 18n;
+      const TOKEN_IN = process.env.GOERLI_SPCOIN;
+      const TOKEN_OUT = process.env.GOERLI_WETH;
+      const POOL_FEE = 3000;
+      const AMOUNT_OUT_MINIMUM = 0;
+      const SQRT_ROOT_PRICE_LIMIT_X96 = 0;
+
+      // Deposit TOKEN_IN
+      let tokenInContract = await ethers.getContractAt(TOKEN_IN_ABI, TOKEN_IN);
+      await tokenInContract.connect(accounts[0]).approve(spCoinContract.address, AMOUNT_IN);
+
+      await spCoinExchange.logSwapExactInputSingle(
+        TOKEN_IN_NAME,
+        TOKEN_OUT_NAME,
+        TOKEN_IN,
+        TOKEN_OUT,
+        TOKEN_IN_ABI,
+        TOKEN_OUT_ABI,
+        POOL_FEE,
+        AMOUNT_IN,
+        AMOUNT_OUT_MINIMUM,
+        SQRT_ROOT_PRICE_LIMIT_X96
+      );
+    }).timeout(100000);  
+
 });
