3b17aa1baeea01d97a3b86e878f9a20eabf28e69
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-10-05 02:09:54 -0400

    WIP

---
+++ scripts/priceQuoteTest/AlphaRouterServiceDebug.js
@@ -0,0 +1,76 @@
+require("dotenv").config();
+
+const { AlphaRouterService } = require('./AlphaRouterService')
+
+class AlphaRouterServiceDebug {
+    constructor() {
+      this.ars = new AlphaRouterService()
+      console.log( "DEBUG MODE ON: AlphaRouterServiceDebug");
+  }
+
+    getRoute = async(_recipientAddr, _tokenIn, _tokenOut, _inputAmount, _slippagePercent) => {
+      console.log( " EXECUTING getRoute(", _recipientAddr, _tokenIn, _tokenOut, _inputAmount, _slippagePercent, ")" );
+      let result = await this.ars.getRoute(_recipientAddr, _tokenIn, _tokenOut, _inputAmount, _slippagePercent);
+      console.log( "getRoute result:", result);
+      return result;
+  }
+    
+    getStrPriceQuote = async(_recipientAddr, _tokenIn, _tokenOut, _inputAmount, _slippagePercent, _decimals) => {
+        console.log( " EXECUTING getStrPriceQuote(", _recipientAddr, _tokenIn, _tokenOut, _inputAmount, _slippagePercent, _decimals, ")" );
+        let result = await this.ars.getStrPriceQuote(_recipientAddr, _tokenIn, _tokenOut, _inputAmount, _slippagePercent, _decimals)
+        console.log( "getStrPriceQuote result:", result);
+        return result;
+   }
+    
+    getPriceQuote = async(_recipientAddr, _tokenIn, _tokenOut, _inputAmount, _slippagePercent) => {
+      console.log( " EXECUTING getPriceQuote(", _recipientAddr, _tokenIn, _tokenOut, _inputAmount, _slippagePercent, ")" );
+      let result = await this.ars.getPriceQuote(_recipientAddr, _tokenIn, _tokenOut, _inputAmount, _slippagePercent)
+      console.log( "getPriceQuote result:", result);
+      return result;
+   }
+    
+    getTransaction = ( _route, _walletAddress, _gasLimit ) => {
+      console.log( " EXECUTING getTransaction(", _route, _walletAddress, _gasLimit, ")" );
+      let result = this.ars.getTransaction = ( _route, _walletAddress, _gasLimit )
+      console.log( "getTransaction result:", result);
+      return result;
+   }
+    
+    exeRouteTransaction = async( _walletAddress, _walletPvtKey, _tokenInAddr, _route, _gasLimit) => {
+      console.log( " EXECUTING exeRouteTransaction(", re_walletAddress, _walletPvtKey, _tokenInAddr, _route, _gasLimitsult, ")" );
+      let result = await this.ars.exeRouteTransaction( _walletAddress, _walletPvtKey, _tokenInAddr, _route, _gasLimit)
+      console.log( "exeRouteTransaction result:", result);
+      return result;
+  }
+    
+    exeTransaction = async(
+      _walletAddress,
+      _walletPvtKey,
+      _tokenIn,
+      _tokenOut,
+      _inputAmount,
+      _slippagePercent,
+      _gasLimit) => {
+        console.log( " EXECUTING exeTransaction(", _walletAddress,
+        _walletPvtKey,
+        _tokenIn,
+        _tokenOut,
+        _inputAmount,
+        _slippagePercent,
+        _gasLimit, ")" );
+        let result = await this.ars.exeTransaction(
+          _walletAddress,
+          _walletPvtKey,
+          _tokenIn,
+          _tokenOut,
+          _inputAmount,
+          _slippagePercent,
+          _gasLimit)
+        console.log( "exeTransaction result:", result);
+        return result;
+      }
+}
+
+module.exports = {
+  AlphaRouterServiceDebug
+}
--- scripts/priceQuoteTest/runPriceQuoteTest.js
+++ scripts/priceQuoteTest/runPriceQuoteTest.js
@@ -1,38 +1,43 @@
 require("dotenv").config();
+
+let DEBUG_MODE = false;
  
 const { Token, CurrencyAmount, TradeType, Percent } = require('@uniswap/sdk-core')
 const { ethers } = require('ethers')
 const JSBI  = require('jsbi') // jsbi@3.2.5
-const { AlphaRouterService } = require('./AlphaRouterService')
-
+const { AlphaRouterServiceDebug } = require('./AlphaRouterServiceDebug')
+const { AlphaRouterService } = require('./AlphaRouterService');
+const { lstat } = require("fs");
 
 require('dotenv').config()
 const WALLET_ADDRESS = process.env.WALLET_ADDRESS
 const INFURA_TEST_URL = process.env.GOERLI_INFURA_TEST_URL
 const CHAIN_ID = parseInt(process.env.GORELI_CHAIN_ID)
 const WALLET_SECRET = process.env.WALLET_SECRET
+const ERC20ABI = require('./abi.json')
 
-const web3Provider = new ethers.providers.JsonRpcProvider(INFURA_TEST_URL) // Ropsten
+
+const provider = new ethers.providers.JsonRpcProvider(INFURA_TEST_URL) // Ropsten
 
 const chainId = parseInt(CHAIN_ID)
 
 const name0 = 'Wrapped Ether'
 const symbol0 = 'WETH'
 const decimals0 = 18
-const address0 = process.env.GOERLI_WETH
+const WETH_ADDRESS = process.env.GOERLI_WETH
 
 const name1 = 'Uniswap Token'
 const symbol1 = 'UNI'
 const decimals1 = 18
-const address1 = process.env.GOERLI_UNI
+const UNI_ADDRESS = process.env.GOERLI_UNI
 
-const WETH = new Token(chainId, address0, decimals0, symbol0, name0)
-const UNI = new Token(chainId, address1, decimals1, symbol1, name1)
+const WETH = new Token(chainId, WETH_ADDRESS, decimals0, symbol0, name0)
+const UNI = new Token(chainId, UNI_ADDRESS, decimals1, symbol1, name1)
 
 let wei = ethers.utils.parseUnits('0.01', 18)
 let inputAmount = CurrencyAmount.fromRawAmount(WETH, JSBI.BigInt(wei))
 
-let ars = new AlphaRouterService();
+let ars = DEBUG_MODE ? new AlphaRouterServiceDebug() : new AlphaRouterService();
 
 main = async() => {
     let tokenIn = WETH;
@@ -44,13 +49,16 @@ main = async() => {
     let priceQuote = await ars.getStrPriceQuote(recipient, tokenIn, tokenOut, inputAmount, slippagePercent, decimals)
     console.log("priceQuote:", priceQuote);
 
-    decimals = 10
-    priceQuote = await ars.getStrPriceQuote(recipient, tokenIn, tokenOut, inputAmount, slippagePercent, decimals)
-    console.log("priceQuote:", priceQuote);
+    console.log("WETH_ADDRESS:", WETH_ADDRESS);
+    console.log("ERC20ABI:", ERC20ABI);
+    console.log("provider:", provider);
 
-    decimals = 7
-    priceQuote = await ars.getStrPriceQuote(recipient, tokenIn, tokenOut, inputAmount, slippagePercent, decimals)
-    console.log("priceQuote:", priceQuote);
+    const WETH_CONTRACT = new ethers.Contract(WETH_ADDRESS, ERC20ABI, provider)
+    console.log("WETH_CONTRACT.balanceOf", (await WETH_CONTRACT.balanceOf(WALLET_ADDRESS)).toString());
+
+    const UNI_CONTRACT = new ethers.Contract(UNI_ADDRESS, ERC20ABI, provider)
+    console.log("UNI_CONTRACT.balanceOf", (await UNI_CONTRACT.balanceOf(WALLET_ADDRESS)).toString());
+
 }
 
 main()
